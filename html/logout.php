5ca1ab00ecc9032349d914153182a434dc423c3b
ShunsukeTanizaki <s21valley@gmail.com>
2019-08-09 19:08:11 +0900

    ログアウト不具合修正

--- html/logout.php
+++ html/logout.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-$_SESSION = array();
-if (ini_set('session_use_cookies')) {
+$_SESSION = array(); //空の配列で上書き
+if (ini_get('session_use_cookies')) {
   $params = session_get_cookie_params();
   setcookie(session_name() . '', time() - 42000, 
     $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
-session_destroy();
+session_destroy();//セッション削除
 
 setcookie('email', '', time()-3600);
 
